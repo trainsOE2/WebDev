219053642f67f5274b15284e4e08988a8b3c7625
trainsOE2 <meghasingh.ms@gmail.com>
2017-08-07 14:33:35 +0530

    Adding .js file for Delete option

--- ToDo List/todo.js
+++ ToDo List/todo.js
@@ -1,15 +1,34 @@
 var tasks = [];
 var input = prompt("What would you like to do?");
-while(input != "quit")
+while(input != "Quit")
 {
-  if (input === "new")
-  {
-    var newTask = prompt("Enter a new Task");
-    tasks.push(newTask);
-  }
-  else if(input === "list")
-  {
-    console.log(tasks);
-  }
+  if (input ===   "New")
+    addTasks();
+  else if(input === "List")
+    listTasks();
+  else if(input === "Delete")
+    deleteTasks();
   input = prompt("What would you like to do?");
+}
+
+
+function addTasks()
+{
+  var newTask = prompt("Enter a new Task");
+  tasks.push(newTask);
+  console.log("Added Task");
+}
+
+function listTasks()
+{
+    tasks.forEach(function(task, i){
+      console.log(i + ": " + task);
+    })
+}
+
+function deleteTasks()
+{
+  var index = prompt("Index of task to be deleted?")
+  tasks.splice(index, 1);
+  console.log("Deleted Task");
 }
