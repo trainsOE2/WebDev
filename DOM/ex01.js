d9d26d44ea99268f7042dae06cd8b61f0f75cfb0
trainsOE2 <meghasingh.ms@gmail.com>
2017-09-03 17:02:29 +0530

    Adding JS file

---
+++ DOM/ex01.js
@@ -0,0 +1,9 @@
+console.log("Test JS");
+var para = document.querySelector("#first");
+console.log(para);
+var par = document.getElementById("first");
+console.log(par);
+var pa = document.querySelector(".special")
+console.log(pa);
+var p = document.getElementsByClassName("special")[0];
+console.log(p);
