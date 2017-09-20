5a370fd6e615355d962e0f8713582cd86fa5dc28
trainsOE2 <meghasingh.ms@gmail.com>
2017-09-20 23:17:17 +0530

    updating JS file

--- The Color Game Project/CG.js
+++ The Color Game Project/CG.js
@@ -5,15 +5,19 @@ function colorGenerator(){
 }
 
 var colors = [];
+colors.length = squares.length;
 var random = colorGenerator();
 
+
 for (var i=0 ; i < squares.length ; ++i){
-   colors[i] = rgb(random, random, random);
+   colors[i] = "rgb(90, 90, 90)";
    console.log(colors[i]);
    squares[i].style.background = colors[i];
+   squares[i].addEventListener("click", function(){
+     alert(this.style.background);
+   })
 }
-
 var pickedSquare = Math.floor(Math.random() * 6) + 1
 var pickedColor = colors[pickedSquare];
 var rgb = document.getElementById('colorDisplay');
-rgb.textContent = "RGB(" + pickedColor + ")";
+rgb.textContent = pickedColor;
