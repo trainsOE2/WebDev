3d37d69fa288bb9bca334a208ed884d01d60784a
trainsOE2 <meghasingh.ms@gmail.com>
2017-12-04 13:35:01 +0530

    Backend Exercise beginner

--- Backend_Node/IntroToNode/grader.js
+++ Backend_Node/IntroToNode/grader.js
@@ -1,13 +1,15 @@
 function grader(arr) {
   var sum=0;
   var average;
-  for(var i=0 ; i<arr.length ; i++){
-    sum += arr[i];
-  }
+  arr.forEach(function (arrEle) {
+    sum += arrEle;
+  })
   average = sum/arr.length;
-  var avg = Math.ceil(average);
-  console.log("Average score is: " + avg);
+  return Math.round(average);
 }
 
 var scores = [90, 98, 89, 100, 100, 86, 94];
-grader(scores);
+console.log("Average score for setA: " + grader(scores));
+
+var scores = [81, 82, 99, 86, 74, 88, 91, 82];
+console.log("Average score for setB: " + grader(scores));
