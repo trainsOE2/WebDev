61c643500e6bd0b640a02a71e18ffbd3e6f4dbb6
trainsOE2 <meghasingh.ms@gmail.com>
2017-12-06 21:19:06 +0530

    added modified expressApp.js

--- Backend_Node/IntroToNPM/ExpressDemo/expressApp.js
+++ Backend_Node/IntroToNPM/ExpressDemo/expressApp.js
@@ -1,10 +1,24 @@
 var express = require("express");
 var app = express();
 
+var bodyParser = require("body-parser");
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: false}));
+var friends = ["Akku", "Alankrith", "Amorthyo", "Ankita", "Haripriya", "Samyak", "Shreyas"];
 
 app.get("/", function(req, res){
   res.render("home");
+});
+
+app.post("/addFriend", function(req, res){
+  var newFriend = req.body.newFriend;
+  friends.push(newFriend);
+  res.redirect("/friends");
+});
+
+app.get("/friends", function(req, res){
+  res.render("friends", {friends: friends});
 });
 
 app.listen(3000, function(){
