c657553deecef833cc6b392a03375195840c0161
trainsOE2 <meghasingh.ms@gmail.com>
2017-12-10 19:40:31 +0530

    Relocating YelpCamp

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -9,7 +9,8 @@ app.set("view engine", "ejs");
 
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -17,7 +18,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //   {
 //     name: "Riverside Walks",
-//     image: "http://nomanbefore.com/wp-content/uploads/2017/07/JoshuaTree_Summer17_WEB-8770-1024x683.jpg"
+//     image: "http://nomanbefore.com/wp-content/uploads/2017/07/JoshuaTree_Summer17_WEB-8770-1024x683.jpg",
+//     description: "No water, no bathrooms, beautiful mountaintop view, absolute bliss!!!"
 //   }, function(err, campground){
 //     if(err){
 //       console.log(err);
@@ -47,7 +49,8 @@ app.get("/campgrounds", function(req, res){
 app.post("/campgrounds", function(req, res){
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image};
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
   Campground.create(newCampground, function(err, newlyCreated){
     if(err){
       console.log(err);
@@ -64,7 +67,13 @@ app.get("/campgrounds/new", function(req, res){
 
 app.get("/campgrounds/:id", function(req, res){
   //res.send("This will be the SHOW page one day!");
-  res.render("show.ejs");
+  Campground.findById(req.params.id, function(err, foundCampground){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("show", {campground: foundCampground})
+    }
+  });
 })
 
 app.listen(3000, function(req, res){
