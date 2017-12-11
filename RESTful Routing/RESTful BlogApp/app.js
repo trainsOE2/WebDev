3052cbb0247bc2e3c08471957e1ec2b1bff00462
trainsOE2 <meghasingh.ms@gmail.com>
2017-12-11 20:35:52 +0530

    created app.js with root route

---
+++ RESTful Routing/RESTful BlogApp/app.js
@@ -0,0 +1,8 @@
+var express     =    require('express'),
+    app         =    express(),
+    bodyParser  =    require('body-parser'),
+    mongoose    =    require('mongoose');
+
+app.listen(3000, function(req, res){
+  
+})
