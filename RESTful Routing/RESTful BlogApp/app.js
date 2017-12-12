120630b68ffd6f52f844ffcfedecc6a6558bca03
trainsOE2 <meghasingh.ms@gmail.com>
2017-12-12 11:47:07 +0530

    Added databse schema and other routes

--- RESTful Routing/RESTful BlogApp/app.js
+++ RESTful Routing/RESTful BlogApp/app.js
@@ -3,6 +3,20 @@ var express     =    require('express'),
     bodyParser  =    require('body-parser'),
     mongoose    =    require('mongoose');
 
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded{extended: true});
+app.use(express.static("public"));
+
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("")
+
 app.listen(3000, function(req, res){
-  
-})
+  console.log("Server is listening!");
+});
