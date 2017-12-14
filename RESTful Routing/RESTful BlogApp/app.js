8c37c111edf6f16935d494dd2e15fbfb19618396
trainsOE2 <meghasingh.ms@gmail.com>
2017-12-14 21:03:57 +0530

    Edit, Show, Update routes

--- RESTful Routing/RESTful BlogApp/app.js
+++ RESTful Routing/RESTful BlogApp/app.js
@@ -1,12 +1,14 @@
-var express     =    require('express'),
-    app         =    express(),
-    bodyParser  =    require('body-parser'),
-    mongoose    =    require('mongoose');
+var express          =    require('express'),
+    app              =    express(),
+    bodyParser       =    require('body-parser'),
+    mongoose         =    require('mongoose');
+    methodOverride   =    require('method-override');
 
 mongoose.connect("mongodb://localhost/restfulBlog_app");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL Config
 var blogSchema = new mongoose.Schema({
@@ -24,20 +26,65 @@ var Blog = mongoose.model("Blog", blogSchema);
 // });
 
 //RESTful Routes
+
+//Landing
 app.get("/", function(req, res){
   res.redirect("/blogs");
 });
 
+//Index
 app.get("/blogs", function(req, res){
   Blog.find({}, function(err, blogs){
     if(err){
       console.log("ERROR :(");
-    }
-    else{
+    } else{
       res.render("index", {blogs: blogs});
     }
   })
 });
+
+//New
+app.get("/blogs/new", function(req, res){
+  res.render("new");
+});
+
+//Create
+app.post("/blogs", function(req, res){
+  Blog.create(req.body.blog, function(err, newBlog){
+    if(err){
+      res.render("new");
+    } else{
+      res.redirect("/blogs");
+    }
+  });
+});
+
+//Show
+app.get("/blogs/:id", function(req, res){
+  Blog.findById(req.params.id, function(err, foundBlog){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.render("show", {blog: foundBlog});
+    }
+  })
+});
+
+// Edit
+ app.get("/blogs/:id/edit", function(req, res){
+   Blog.findById(req.params.id, function(err, foundBlog){
+     if(err){
+       res.redirect("/blogs");
+     } else {
+       res.render("edit", {blog: foundBlog});
+     }
+   })
+ });
+
+//Update
+app.put("/blogs/:id", function(req, res){
+  res.send("Update Route!");
+})
 
 app.listen(3000, function(req, res){
   console.log("Server is listening!");
