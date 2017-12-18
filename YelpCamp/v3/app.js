b3968b00c5bb1c7222de0a8bbfd2646ca1e9ea5e
trainsOE2 <meghasingh.ms@gmail.com>
2017-12-18 20:23:59 +0530

    display comments and created v4

---
+++ YelpCamp/v3/app.js
@@ -0,0 +1,62 @@
+var express      = require('express'),
+    app          = express(),
+    bodyParser   = require('body-parser'),
+    mongoose     = require('mongoose'),
+    Campground   = require('./models/campground'),
+    seedDB       = require('./seeds')
+
+
+mongoose.connect("mongodb://localhost/yelp_camp_v3");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+seedDB();
+
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err);
+    } else{
+      res.render("index", {campgrounds:allCampgrounds});
+    }
+  });
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
+  Campground.create(newCampground, function(err, newlyCreated){
+    if(err){
+      console.log(err);
+    } else{
+      res.redirect("/campgrounds");
+    }
+  });
+
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+  //res.send("This will be the SHOW page one day!");
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+    if(err){
+      console.log(err);
+    } else {
+      console.log(foundCampground);
+      res.render("show", {campground: foundCampground})
+    }
+  });
+});
+
+app.listen(3000, function(req, res){
+  console.log("The YelpCamp Server has started!");
+});
