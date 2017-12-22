7feefcb3bed0cc43107e9d8d5ff5125cb28eb14e
trainsOE2 <meghasingh.ms@gmail.com>
2017-12-22 22:26:29 +0530

    required everything required

--- DeployingDemo/app.js
+++ DeployingDemo/app.js
@@ -1,6 +1,13 @@
-var express = require("express");
-var app = express();
+var express                = require("express"),
+    mongoose               = require("mongoose"),
+    passport               = require("passport"),
+    bodyParser             = require("body-parser"),
+    localStrategy          = require("passport-local"),
+    passportLocalMongoose  = require("passport-local-mongoose");
 
+mongoose.connect("mongodb://localhost/auth_demo");
+
+var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
@@ -9,6 +16,10 @@ app.get("/", function(req, res){
 
 app.get("/about", function(req, res){
   res.render("about");
+});
+
+app.get("/secret", function(req, res){
+  res.sender("secret");
 });
 
 app.listen(3000, function(req, res){
